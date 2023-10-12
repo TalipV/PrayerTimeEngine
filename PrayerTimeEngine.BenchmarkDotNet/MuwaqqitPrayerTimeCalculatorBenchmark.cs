649512dca57dc5e711caeada8e91077f28ef5122
talip <talip.vu@gmail.com>
2023-10-12 22:12:10 +0200

    add basic profiling example with BenchmarkDotNet

---
+++ PrayerTimeEngine.BenchmarkDotNet/MuwaqqitPrayerTimeCalculatorBenchmark.cs
@@ -0,0 +1,82 @@
+﻿using BenchmarkDotNet.Attributes;
+using Microsoft.Extensions.DependencyInjection;
+using NodaTime;
+using PrayerTimeEngine.Core.Common.Enum;
+using PrayerTimeEngine.Core.Data.SQLite;
+using PrayerTimeEngine.Core.Domain.Calculators.Muwaqqit.Models;
+using PrayerTimeEngine.Core.Domain.Calculators.Muwaqqit.Services;
+using PrayerTimeEngine.Core.Domain.Configuration.Models;
+using PrayerTimeEngine.Core.Tests.API.MuwaqqitAPI;
+using SQLitePCL;
+
+namespace PrayerTimeEngine.BenchmarkDotNet
+{
+    [MemoryDiagnoser]
+    public class MuwaqqitPrayerTimeCalculatorBenchmark
+    {
+        private static SQLiteDB _sqlite = null;
+        private static MuwaqqitPrayerTimeCalculator _muwaqqitPrayerTimeCalculator = null;
+        private static List<GenericSettingConfiguration> _configs =
+            new()
+            {
+                    new MuwaqqitDegreeCalculationConfiguration { TimeType = ETimeType.FajrStart, Degree = -12.0 },
+                    new GenericSettingConfiguration { TimeType = ETimeType.FajrEnd, Source = ECalculationSource.Muwaqqit },
+                    new MuwaqqitDegreeCalculationConfiguration { TimeType = ETimeType.FajrGhalas, Degree = -7.5 },
+                    new MuwaqqitDegreeCalculationConfiguration { TimeType = ETimeType.FajrKaraha, Degree = -4.5 },
+                    new MuwaqqitDegreeCalculationConfiguration { TimeType = ETimeType.DuhaStart, Degree = 3.5 },
+                    new GenericSettingConfiguration { TimeType = ETimeType.DhuhrStart, Source = ECalculationSource.Muwaqqit },
+                    new GenericSettingConfiguration { TimeType = ETimeType.DhuhrEnd, Source = ECalculationSource.Muwaqqit },
+                    new GenericSettingConfiguration { TimeType = ETimeType.AsrStart, Source = ECalculationSource.Muwaqqit },
+                    new GenericSettingConfiguration { TimeType = ETimeType.AsrEnd, Source = ECalculationSource.Muwaqqit },
+                    new GenericSettingConfiguration { TimeType = ETimeType.AsrMithlayn, Source = ECalculationSource.Muwaqqit },
+                    new MuwaqqitDegreeCalculationConfiguration { TimeType = ETimeType.AsrKaraha, Degree = 4.5 },
+                    new GenericSettingConfiguration { TimeType = ETimeType.MaghribStart, Source = ECalculationSource.Muwaqqit },
+                    new MuwaqqitDegreeCalculationConfiguration { TimeType = ETimeType.MaghribEnd, Degree = -12.0 },
+                    new MuwaqqitDegreeCalculationConfiguration { TimeType = ETimeType.MaghribIshtibaq, Degree = -8 },
+                    new MuwaqqitDegreeCalculationConfiguration { TimeType = ETimeType.IshaStart, Degree = -15.5 },
+                    new MuwaqqitDegreeCalculationConfiguration { TimeType = ETimeType.IshaEnd, Degree = -15.0 },
+            };
+        private static MuwaqqitLocationData _locationData =
+            new MuwaqqitLocationData
+            {
+                Latitude = 47.2803835M,
+                Longitude = 11.41337M,
+                TimezoneName = "Europe/Vienna"
+            };
+
+        [GlobalSetup]
+        public void Setup()
+        {
+            _sqlite = MuwaqqitPrayerTimeCalculatorTests.ServiceProvider.GetService<ISQLiteDB>() as SQLiteDB;
+            _muwaqqitPrayerTimeCalculator = MuwaqqitPrayerTimeCalculatorTests.ServiceProvider.GetService<MuwaqqitPrayerTimeCalculator>();
+        }
+
+        private static Microsoft.Data.Sqlite.SqliteConnection _sqlConnection;
+
+        [IterationSetup]
+        public void IterationSetup()
+        {
+            _sqlConnection = _sqlite.GetSqliteConnection("Data Source=:memory:");
+            _sqlite.InitializeDatabase(filePathDatabase: false);
+        }
+
+        [IterationCleanup]
+        public void IterationCleanup()
+        {
+            _sqlConnection?.Close();
+            _sqlConnection?.Dispose();
+        }
+
+        [Benchmark]
+        public void Test1()
+        {
+            for(int i = 0; i < 1000; i++)
+            {
+                var result = _muwaqqitPrayerTimeCalculator.GetPrayerTimesAsync(
+                    date: new LocalDate(2023, 7, 30),
+                    locationData: _locationData,
+                    configurations: _configs);
+            }
+        }
+    }
+}
---
+++ PrayerTimeEngine.BenchmarkDotNet/Program.cs
@@ -0,0 +1,12 @@
+﻿using BenchmarkDotNet.Running;
+
+namespace PrayerTimeEngine.BenchmarkDotNet
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var summary = BenchmarkRunner.Run<MuwaqqitPrayerTimeCalculatorBenchmark>();
+        }
+    }
+}
