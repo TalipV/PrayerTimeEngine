6f7bc183dfdc4800822c2d1386cb64794d3b84f3
talip <talip.vu@gmail.com>
2023-11-11 08:42:13 +0100

    add basic code for android notification

--- PrayerTimeEngine.MAUI/Platforms/Android/MainActivity.cs
+++ PrayerTimeEngine.MAUI/Platforms/Android/MainActivity.cs
@@ -12,4 +12,21 @@ public class MainActivity : MauiAppCompatActivity
         base.OnCreate(savedInstanceState);
     }
 
+    void createNotificationChannels()
+    {
+        if (OperatingSystem.IsAndroidVersionAtLeast(26))
+        {
+            string name = "Prayer Time Notifications";
+            string description = "Updates and reminders for upcoming prayer times.";
+            string channelId = "prayer_time_channel";
+
+            var channel = new NotificationChannel(channelId, name, NotificationImportance.Default)
+            {
+                Description = description
+            };
+
+            var notificationManager = (NotificationManager)GetSystemService(NotificationService);
+            notificationManager.CreateNotificationChannel(channel);
+        }
+    }
 }
---
+++ PrayerTimeEngine.MAUI/Platforms/Android/NotificationService.cs
@@ -0,0 +1,39 @@
+﻿using Android.App;
+using Android.Content;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrayerTimeEngine.Platforms.Android
+{
+    public class NotificationService
+    {
+        public void ShowNotification(string title, string content)
+        {
+            var context = Microsoft.Maui.Essentials.Platform.CurrentActivity;
+
+
+            var notificationManager = context.GetSystemService(Context.NotificationService) as NotificationManager;
+
+            // Create the intent to launch your application when the user taps the notification
+            Intent intent = context.PackageManager.GetLaunchIntentForPackage(context.PackageName);
+            PendingIntent pendingIntent = PendingIntent.GetActivity(context, 0, intent, 0);
+
+            // Use the same channel ID as defined in MainActivity
+            var channelId = "prayer_time_channel";
+
+            // Build the notification
+            var notificationBuilder = new Notification.Builder(context, channelId)
+                .SetContentTitle(title)
+                .SetContentText(content)
+                .SetContentIntent(pendingIntent)
+                .SetAutoCancel(true);
+
+            // Notify
+            notificationManager.Notify(0, notificationBuilder.Build());
+        }
+
+    }
+}
