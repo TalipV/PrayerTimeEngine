e76a3c67c5dc1b289caed465cf3b0b85f23938c7
talip <talip.vu@gmail.com>
2024-02-13 22:25:30 +0100

    .

--- PrayerTimeEngine.Core.Tests/Domain/Calculators/Muwaqqit/MuwaqqitApiServiceTests.cs
+++ PrayerTimeEngine.Core.Tests/Domain/Calculators/Muwaqqit/MuwaqqitApiServiceTests.cs
@@ -1,13 +1,84 @@
-﻿using PrayerTimeEngine.Core.Tests.Common;
+﻿using FluentAssertions;
+using NodaTime;
+using PrayerTimeEngine.Core.Domain.Calculators.Muwaqqit.Models;
+using PrayerTimeEngine.Core.Domain.Calculators.Muwaqqit.Services;
+using PrayerTimeEngine.Core.Domain.Calculators.Semerkand.Interfaces;
+using PrayerTimeEngine.Core.Domain.Calculators.Semerkand.Services;
+using PrayerTimeEngine.Core.Tests.Common;
+using System.Net;
 
 namespace PrayerTimeEngine.Core.Tests.Unit.Domain.Calculators.Muwaqqit
 {
     public class MuwaqqitApiServiceTests : BaseTest
     {
-        [Fact]
-        public void X_X_X()
+        private readonly MockHttpMessageHandler _mockHttpMessageHandler;
+        private readonly MuwaqqitApiService _muwaqqitApiService;
+
+        public MuwaqqitApiServiceTests()
         {
-            throw new NotImplementedException();
+            _mockHttpMessageHandler = new MockHttpMessageHandler();
+            HttpClient httpClient = new HttpClient(_mockHttpMessageHandler);
+            _muwaqqitApiService = new MuwaqqitApiService(httpClient);
+        }
+
+        [Fact]
+        public async Task GetTimesAsync_X_X()
+        {
+            // ARRANGE
+            LocalDate date = new LocalDate(2023, 7, 29);
+            DateTimeZone europeTimeZone = DateTimeZoneProviders.Tzdb["Europe/Vienna"];
+
+            _mockHttpMessageHandler.HandleRequestFunc =
+                (request) =>
+                {
+                    string responseText =
+                        File.ReadAllText(
+                            Path.Combine(
+                                TEST_DATA_FILE_PATH,
+                                "MuwaqqitTestData",
+                                "Muwaqqit_TestPrayerTimeData_20230730_Innsbruck_Config1.txt"));
+                    return new HttpResponseMessage
+                    {
+                        StatusCode = HttpStatusCode.OK,
+                        Content = new StringContent(responseText)
+                    };
+                };
+
+            // ACT
+            MuwaqqitPrayerTimes time = await _muwaqqitApiService.GetTimesAsync(
+                date: date,
+                longitude: 1M,
+                latitude: 1M,
+                fajrDegree: 1,
+                ishaDegree: 1,
+                ishtibaqDegree: 1,
+                asrKarahaDegree: 1,
+                timezone: europeTimeZone.Id);
+
+            // ASSERT
+            time.Should().NotBeNull();
+
+            time.ID.Should().Be(0);
+            time.Date.Should().Be(new LocalDate(2023, 7, 30));
+            time.FajrDegree.Should().Be(1);
+            time.AsrKarahaDegree.Should().Be(1);
+            time.IshtibaqDegree.Should().Be(1);
+            time.IshaDegree.Should().Be(1);
+            time.Latitude.Should().Be(47.2803835M);
+            time.Longitude.Should().Be(11.41337M);
+            time.InsertInstant.Should().BeNull();
+
+            time.Asr.Should().Be(new ZonedDateTime(Instant.FromUtc(2023, 7, 30, 15, 25, 53), europeTimeZone));
+            time.AsrKaraha.Should().Be(new ZonedDateTime(Instant.FromUtc(2023, 7, 30, 18, 23, 53), europeTimeZone));
+            time.AsrMithlayn.Should().Be(new ZonedDateTime(Instant.FromUtc(2023, 7, 30, 16, 33, 27), europeTimeZone));
+            time.Dhuhr.Should().Be(new ZonedDateTime(Instant.FromUtc(2023, 7, 30, 11, 21, 22), europeTimeZone));
+            time.Duha.Should().Be(new ZonedDateTime(Instant.FromUtc(2023, 7, 30, 4, 17, 04), europeTimeZone));
+            time.Fajr.Should().Be(new ZonedDateTime(Instant.FromUtc(2023, 7, 30, 2, 27, 04), europeTimeZone));
+            time.Isha.Should().Be(new ZonedDateTime(Instant.FromUtc(2023, 7, 30, 20, 13, 17), europeTimeZone));
+            time.Ishtibaq.Should().Be(new ZonedDateTime(Instant.FromUtc(2023, 7, 30, 19, 41, 46), europeTimeZone));
+            time.Maghrib.Should().Be(new ZonedDateTime(Instant.FromUtc(2023, 7, 30, 18, 50, 59), europeTimeZone));
+            time.NextFajr.Should().Be(new ZonedDateTime(Instant.FromUtc(2023, 7, 31, 2, 28, 47), europeTimeZone));
+            time.Shuruq.Should().Be(new ZonedDateTime(Instant.FromUtc(2023, 7, 30, 3, 49, 53), europeTimeZone));
         }
     }
 }
--- PrayerTimeEngine.Core.Tests/Domain/Calculators/Muwaqqit/MuwaqqitDBAccessTests.cs
+++ PrayerTimeEngine.Core.Tests/Domain/Calculators/Muwaqqit/MuwaqqitDBAccessTests.cs
@@ -1,12 +1,100 @@
-﻿using PrayerTimeEngine.Core.Tests.Common;
+﻿using FluentAssertions;
+using NodaTime;
+using PrayerTimeEngine.Core.Data.EntityFramework;
+using PrayerTimeEngine.Core.Domain.Calculators.Muwaqqit.Models;
+using PrayerTimeEngine.Core.Domain.Calculators.Muwaqqit.Services;
+using PrayerTimeEngine.Core.Tests.Common;
 
 namespace PrayerTimeEngine.Core.Tests.Unit.Domain.Calculators.Muwaqqit
 {
     public class MuwaqqitDBAccessTests : BaseTest
     {
-        [Fact]public void X_X_X()
+        private readonly AppDbContext _appDbContext;
+        private readonly MuwaqqitDBAccess _muwaqqitDBAccess;
+
+        public MuwaqqitDBAccessTests()
         {
-            throw new NotImplementedException();
+            _appDbContext = GetHandledDbContext();
+            _muwaqqitDBAccess = new MuwaqqitDBAccess(_appDbContext);
+        }
+
+        [Fact]
+        public async Task GetTimesAsync_ExistingTime_ReturnsCorrectTime()
+        {
+            // ARRANGE
+            LocalDate date = new LocalDate(2023, 7, 30);
+            var muwaqqitTime = new MuwaqqitPrayerTimes
+            {
+                Date = date,
+                Latitude = 47.2803835M,
+                Longitude = 11.41337M,
+                FajrDegree = 1,
+                IshaDegree = 1,
+                IshtibaqDegree = 1,
+                AsrKarahaDegree = 1,
+                Fajr = new ZonedDateTime(Instant.FromUtc(2023, 7, 30, 2, 27, 04), DateTimeZoneProviders.Tzdb["Europe/Vienna"]),
+                NextFajr = new ZonedDateTime(Instant.FromUtc(2023, 7, 31, 2, 28, 04), DateTimeZoneProviders.Tzdb["Europe/Vienna"]),
+                Shuruq = new ZonedDateTime(Instant.FromUtc(2023, 7, 30, 3, 49, 53), DateTimeZoneProviders.Tzdb["Europe/Vienna"]),
+                Duha = new ZonedDateTime(Instant.FromUtc(2023, 7, 30, 4, 49, 53), DateTimeZoneProviders.Tzdb["Europe/Vienna"]),
+                Dhuhr = new ZonedDateTime(Instant.FromUtc(2023, 7, 30, 11, 21, 22), DateTimeZoneProviders.Tzdb["Europe/Vienna"]),
+                Asr = new ZonedDateTime(Instant.FromUtc(2023, 7, 30, 15, 25, 53), DateTimeZoneProviders.Tzdb["Europe/Vienna"]),
+                AsrMithlayn = new ZonedDateTime(Instant.FromUtc(2023, 7, 30, 16, 25, 53), DateTimeZoneProviders.Tzdb["Europe/Vienna"]),
+                AsrKaraha = new ZonedDateTime(Instant.FromUtc(2023, 7, 30, 17, 25, 53), DateTimeZoneProviders.Tzdb["Europe/Vienna"]),
+                Maghrib = new ZonedDateTime(Instant.FromUtc(2023, 7, 30, 18, 50, 59), DateTimeZoneProviders.Tzdb["Europe/Vienna"]),
+                Ishtibaq = new ZonedDateTime(Instant.FromUtc(2023, 7, 30, 19, 50, 59), DateTimeZoneProviders.Tzdb["Europe/Vienna"]),
+                Isha = new ZonedDateTime(Instant.FromUtc(2023, 7, 30, 20, 13, 17), DateTimeZoneProviders.Tzdb["Europe/Vienna"])
+            };
+
+            await _appDbContext.MuwaqqitPrayerTimes.AddAsync(muwaqqitTime);
+            await _appDbContext.SaveChangesAsync();
+
+            // ACT
+            var retrievedTime = await _muwaqqitDBAccess.GetTimesAsync(
+                muwaqqitTime.Date,
+                muwaqqitTime.Longitude,
+                muwaqqitTime.Latitude,
+                muwaqqitTime.FajrDegree,
+                muwaqqitTime.IshaDegree,
+                muwaqqitTime.IshtibaqDegree,
+                muwaqqitTime.AsrKarahaDegree);
+
+            // ASSERT
+            retrievedTime.Should().BeEquivalentTo(muwaqqitTime, options => options.IgnoringCyclicReferences());
+        }
+
+        [Fact]
+        public async Task InsertMuwaqqitPrayerTimesAsync_InsertsNewTime()
+        {
+            // ARRANGE
+            LocalDate date = new LocalDate(2023, 7, 31);
+            var newMuwaqqitTime = new MuwaqqitPrayerTimes
+            {
+                Date = date,
+                Latitude = 47.2803835M,
+                Longitude = 11.41337M,
+                FajrDegree = 1,
+                IshaDegree = 1,
+                IshtibaqDegree = 1,
+                AsrKarahaDegree = 1,
+                Fajr = new ZonedDateTime(Instant.FromUtc(2023, 7, 30, 2, 27, 04), DateTimeZoneProviders.Tzdb["Europe/Vienna"]),
+                NextFajr = new ZonedDateTime(Instant.FromUtc(2023, 7, 31, 2, 28, 04), DateTimeZoneProviders.Tzdb["Europe/Vienna"]),
+                Shuruq = new ZonedDateTime(Instant.FromUtc(2023, 7, 30, 3, 49, 53), DateTimeZoneProviders.Tzdb["Europe/Vienna"]),
+                Duha = new ZonedDateTime(Instant.FromUtc(2023, 7, 30, 4, 49, 53), DateTimeZoneProviders.Tzdb["Europe/Vienna"]),
+                Dhuhr = new ZonedDateTime(Instant.FromUtc(2023, 7, 30, 11, 21, 22), DateTimeZoneProviders.Tzdb["Europe/Vienna"]),
+                Asr = new ZonedDateTime(Instant.FromUtc(2023, 7, 30, 15, 25, 53), DateTimeZoneProviders.Tzdb["Europe/Vienna"]),
+                AsrMithlayn = new ZonedDateTime(Instant.FromUtc(2023, 7, 30, 16, 25, 53), DateTimeZoneProviders.Tzdb["Europe/Vienna"]),
+                AsrKaraha = new ZonedDateTime(Instant.FromUtc(2023, 7, 30, 17, 25, 53), DateTimeZoneProviders.Tzdb["Europe/Vienna"]),
+                Maghrib = new ZonedDateTime(Instant.FromUtc(2023, 7, 30, 18, 50, 59), DateTimeZoneProviders.Tzdb["Europe/Vienna"]),
+                Ishtibaq = new ZonedDateTime(Instant.FromUtc(2023, 7, 30, 19, 50, 59), DateTimeZoneProviders.Tzdb["Europe/Vienna"]),
+                Isha = new ZonedDateTime(Instant.FromUtc(2023, 7, 30, 20, 13, 17), DateTimeZoneProviders.Tzdb["Europe/Vienna"])
+            };
+
+            // ACT
+            await _muwaqqitDBAccess.InsertMuwaqqitPrayerTimesAsync(newMuwaqqitTime);
+
+            // ASSERT
+            var insertedTime = await _appDbContext.MuwaqqitPrayerTimes.FindAsync(newMuwaqqitTime.ID);
+            insertedTime.Should().BeEquivalentTo(newMuwaqqitTime, options => options.IgnoringCyclicReferences());
         }
     }
 }
