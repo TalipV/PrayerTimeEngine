9a13ca9c71158d333cc3840473bd4784886ceea6
talip <talip.vu@gmail.com>
2025-03-30 23:51:43 +0200

    new tests + minor changes

--- PrayerTimeEngine.Core.Tests.Common/TestData/SubstitutionHelper.cs
+++ PrayerTimeEngine.Core.Tests.Common/TestData/SubstitutionHelper.cs
@@ -18,20 +18,23 @@ namespace PrayerTimeEngine.Core.Tests.Common.TestData;
 
 public class SubstitutionHelper
 {
+    private const string SEMERKAND_BASE_URL = @"https://semerkandtakvimi.semerkandmobile.com";
+    private const string FAZILET_BASE_URL = @"https://fazilettakvimi.com/api/cms";
+    private const string MUWAQQIT_BASE_URL = @"https://www.muwaqqit.com";
+
     public static ISemerkandApiService GetMockedSemerkandApiService()
     {
         static HttpResponseMessage handleRequestFunc(HttpRequestMessage request)
         {
-            Stream responseStream;
-
-            if (request.RequestUri.AbsoluteUri == "https://semerkandtakvimi.semerkandmobile.com/countries?language=tr")
-                responseStream = File.OpenRead(Path.Combine(TestDataHelper.SEMERKAND_TEST_DATA_FILE_PATH, "Semerkand_TestCountriesData.txt"));
-            else if (request.RequestUri.AbsoluteUri == "https://semerkandtakvimi.semerkandmobile.com/cities?countryID=3")
-                responseStream = File.OpenRead(Path.Combine(TestDataHelper.SEMERKAND_TEST_DATA_FILE_PATH, "Semerkand_TestCityData_Austria.txt"));
-            else if (request.RequestUri.AbsoluteUri == "https://semerkandtakvimi.semerkandmobile.com/salaattimes?cityId=197&year=2023")
-                responseStream = File.OpenRead(Path.Combine(TestDataHelper.SEMERKAND_TEST_DATA_FILE_PATH, "Semerkand_TestPrayerTimeData_20230729_Innsbruck.txt"));
-            else
-                throw new Exception($"No response registered for URL: {request.RequestUri.AbsoluteUri}");
+            Stream responseStream = request.RequestUri.AbsoluteUri switch
+            {
+                $"{SEMERKAND_BASE_URL}/countries?language=tr" => File.OpenRead(Path.Combine(TestDataHelper.SEMERKAND_TEST_DATA_FILE_PATH, "Semerkand_TestCountriesData.txt")),
+                $"{SEMERKAND_BASE_URL}/cities?countryID=3" => File.OpenRead(Path.Combine(TestDataHelper.SEMERKAND_TEST_DATA_FILE_PATH, "Semerkand_TestCityData_Austria.txt")),
+                $"{SEMERKAND_BASE_URL}/cities?countryID=2" => File.OpenRead(Path.Combine(TestDataHelper.SEMERKAND_TEST_DATA_FILE_PATH, "Semerkand_TestCityData_Germany.txt")),
+                $"{SEMERKAND_BASE_URL}/salaattimes?cityId=197&year=2023" => File.OpenRead(Path.Combine(TestDataHelper.SEMERKAND_TEST_DATA_FILE_PATH, "Semerkand_TestPrayerTimeData_20230729_Innsbruck.txt")),
+                $"{SEMERKAND_BASE_URL}/salaattimes?cityId=786&year=2025" => File.OpenRead(Path.Combine(TestDataHelper.SEMERKAND_TEST_DATA_FILE_PATH, "Semerkand_TestPrayerTimeData_20250330_Leverkusen.txt")),
+                _ => throw new Exception($"No response registered for URL: {request.RequestUri.AbsoluteUri}"),
+            };
 
             return new HttpResponseMessage
             {
@@ -53,10 +56,10 @@ public class SubstitutionHelper
             Stream responseStream =
                 request.RequestUri.AbsoluteUri switch
                 {
-                    @"https://www.muwaqqit.com/api2.json?d=2023-07-30&ln=11.41337&lt=47.2803835&tz=Europe%2FVienna&fa=-12&ia=3.5&isn=-8&ea=-12" => File.OpenRead(Path.Combine(TestDataHelper.MUWAQQIT_TEST_DATA_FILE_PATH, "Muwaqqit_TestPrayerTimeData_20230730_Innsbruck_Config1.txt")),
-                    @"https://www.muwaqqit.com/api2.json?d=2023-07-30&ln=11.41337&lt=47.2803835&tz=Europe%2FVienna&fa=-7.5&ia=4.5&isn=-12&ea=-15.5" => File.OpenRead(Path.Combine(TestDataHelper.MUWAQQIT_TEST_DATA_FILE_PATH, "Muwaqqit_TestPrayerTimeData_20230730_Innsbruck_Config2.txt")),
-                    @"https://www.muwaqqit.com/api2.json?d=2023-07-30&ln=11.41337&lt=47.2803835&tz=Europe%2FVienna&fa=-4.5&ia=-12&isn=-12&ea=-12" => File.OpenRead(Path.Combine(TestDataHelper.MUWAQQIT_TEST_DATA_FILE_PATH, "Muwaqqit_TestPrayerTimeData_20230730_Innsbruck_Config3.txt")),
-                    @"https://www.muwaqqit.com/api2.json?d=2023-07-30&ln=11.41337&lt=47.2803835&tz=Europe%2FVienna&fa=-15&ia=-12&isn=-12&ea=-12" => File.OpenRead(Path.Combine(TestDataHelper.MUWAQQIT_TEST_DATA_FILE_PATH, "Muwaqqit_TestPrayerTimeData_20230730_Innsbruck_Config4.txt")),
+                    $@"{MUWAQQIT_BASE_URL}/api2.json?d=2023-07-30&ln=11.41337&lt=47.2803835&tz=Europe%2FVienna&fa=-12&ia=3.5&isn=-8&ea=-12" => File.OpenRead(Path.Combine(TestDataHelper.MUWAQQIT_TEST_DATA_FILE_PATH, "Muwaqqit_TestPrayerTimeData_20230730_Innsbruck_Config1.txt")),
+                    $@"{MUWAQQIT_BASE_URL}/api2.json?d=2023-07-30&ln=11.41337&lt=47.2803835&tz=Europe%2FVienna&fa=-7.5&ia=4.5&isn=-12&ea=-15.5" => File.OpenRead(Path.Combine(TestDataHelper.MUWAQQIT_TEST_DATA_FILE_PATH, "Muwaqqit_TestPrayerTimeData_20230730_Innsbruck_Config2.txt")),
+                    $@"{MUWAQQIT_BASE_URL}/api2.json?d=2023-07-30&ln=11.41337&lt=47.2803835&tz=Europe%2FVienna&fa=-4.5&ia=-12&isn=-12&ea=-12" => File.OpenRead(Path.Combine(TestDataHelper.MUWAQQIT_TEST_DATA_FILE_PATH, "Muwaqqit_TestPrayerTimeData_20230730_Innsbruck_Config3.txt")),
+                    $@"{MUWAQQIT_BASE_URL}/api2.json?d=2023-07-30&ln=11.41337&lt=47.2803835&tz=Europe%2FVienna&fa=-15&ia=-12&isn=-12&ea=-12" => File.OpenRead(Path.Combine(TestDataHelper.MUWAQQIT_TEST_DATA_FILE_PATH, "Muwaqqit_TestPrayerTimeData_20230730_Innsbruck_Config4.txt")),
                     _ => throw new Exception($"No response registered for URL: {request.RequestUri.AbsoluteUri}")
                 };
 
@@ -70,7 +73,7 @@ public class SubstitutionHelper
         var mockHttpMessageHandler = new MockHttpMessageHandler(handleRequestFunc);
         var httpClient = new HttpClient(mockHttpMessageHandler)
         {
-            BaseAddress = new Uri(@"https://www.muwaqqit.com/")
+            BaseAddress = new Uri(MUWAQQIT_BASE_URL)
         };
 
         return RestService.For<IMuwaqqitApiService>(httpClient);
@@ -78,20 +81,17 @@ public class SubstitutionHelper
 
     public static IFaziletApiService GetMockedFaziletApiService()
     {
-        string baseURL = @"https://fazilettakvimi.com/api/cms";
-
-        HttpResponseMessage handleRequestFunc(HttpRequestMessage request)
+        static HttpResponseMessage handleRequestFunc(HttpRequestMessage request)
         {
-            Stream responseStream;
-
-            if (request.RequestUri.AbsoluteUri == $@"{baseURL}/daily?districtId=232&lang=1")
-                responseStream = File.OpenRead(Path.Combine(TestDataHelper.FAZILET_TEST_DATA_FILE_PATH, "Fazilet_TestCountriesData.txt"));
-            else if (request.RequestUri.AbsoluteUri == $@"{baseURL}/cities-by-country?districtId=2")
-                responseStream = File.OpenRead(Path.Combine(TestDataHelper.FAZILET_TEST_DATA_FILE_PATH, "Fazilet_TestCityData_Austria.txt"));
-            else if (request.RequestUri.AbsoluteUri == $@"{baseURL}/daily?districtId=92&lang=2")
-                responseStream = File.OpenRead(Path.Combine(TestDataHelper.FAZILET_TEST_DATA_FILE_PATH, "Fazilet_TestPrayerTimeData_20230729_Innsbruck.txt"));
-            else
-                throw new Exception($"No response registered for URL: {request.RequestUri.AbsoluteUri}");
+            Stream responseStream = request.RequestUri.AbsoluteUri switch
+            {
+                $@"{FAZILET_BASE_URL}/daily?districtId=232&lang=1" => File.OpenRead(Path.Combine(TestDataHelper.FAZILET_TEST_DATA_FILE_PATH, "Fazilet_TestCountriesData.txt")),
+                $@"{FAZILET_BASE_URL}/cities-by-country?districtId=2" => File.OpenRead(Path.Combine(TestDataHelper.FAZILET_TEST_DATA_FILE_PATH, "Fazilet_TestCityData_Austria.txt")),
+                $@"{FAZILET_BASE_URL}/cities-by-country?districtId=14" => File.OpenRead(Path.Combine(TestDataHelper.FAZILET_TEST_DATA_FILE_PATH, "Fazilet_TestCityData_Germany.txt")),
+                $@"{FAZILET_BASE_URL}/daily?districtId=92&lang=2" => File.OpenRead(Path.Combine(TestDataHelper.FAZILET_TEST_DATA_FILE_PATH, "Fazilet_TestPrayerTimeData_20230729_Innsbruck.txt")),
+                $@"{FAZILET_BASE_URL}/daily?districtId=2762&lang=2" => File.OpenRead(Path.Combine(TestDataHelper.FAZILET_TEST_DATA_FILE_PATH, "Fazilet_TestPrayerTimeData_20250330_Leverkusen.txt")),
+                _ => throw new Exception($"No response registered for URL: {request.RequestUri.AbsoluteUri}"),
+            };
 
             return new HttpResponseMessage
             {
@@ -101,7 +101,7 @@ public class SubstitutionHelper
         }
 
         var mockHttpMessageHandler = new MockHttpMessageHandler(handleRequestFunc);
-        var httpClient = new HttpClient(mockHttpMessageHandler) { BaseAddress = new Uri(baseURL) };
+        var httpClient = new HttpClient(mockHttpMessageHandler) { BaseAddress = new Uri(FAZILET_BASE_URL) };
 
         return RestService.For<IFaziletApiService>(httpClient);
     }
