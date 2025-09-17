cf1a384b5fb98edf15339f6eaf81c482eb0c3fa4
talip <talip.vu@gmail.com>
2025-09-17 22:50:10 +0200

    DatabaseTablesPageView make all db sets of AppDbContext automatically available

--- PrayerTimeEngine.MAUI/Presentation/Pages/DatabaseTables/DatabaseTablesPageViewModel.cs
+++ PrayerTimeEngine.MAUI/Presentation/Pages/DatabaseTables/DatabaseTablesPageViewModel.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 using PrayerTimeEngine.Core.Data.EntityFramework;
 using PrayerTimeEngine.Presentation.Pages.Settings.SettingsHandler;
 using PropertyChanged;
+using System.Reflection;
 
 namespace PrayerTimeEngine.Presentation.Pages.DatabaseTables;
 
@@ -13,16 +15,7 @@ public partial class DatabaseTablesPageViewModel(
 
     public override void Initialize(params object[] parameter)
     {
-        _dataDict[nameof(AppDbContext.FaziletCountries)] = appDbContext.FaziletCountries.AsNoTracking().ToList().OfType<object>().ToList();
-        _dataDict[nameof(AppDbContext.FaziletCities)] = appDbContext.FaziletCities.AsNoTracking().ToList().OfType<object>().ToList();
-        _dataDict[nameof(AppDbContext.FaziletPrayerTimes)] = appDbContext.FaziletPrayerTimes.AsNoTracking().ToList().OfType<object>().ToList();
-        _dataDict[nameof(AppDbContext.SemerkandCountries)] = appDbContext.SemerkandCountries.AsNoTracking().ToList().OfType<object>().ToList();
-        _dataDict[nameof(AppDbContext.SemerkandCities)] = appDbContext.SemerkandCities.AsNoTracking().ToList().OfType<object>().ToList();
-        _dataDict[nameof(AppDbContext.SemerkandPrayerTimes)] = appDbContext.SemerkandPrayerTimes.AsNoTracking().ToList().OfType<object>().ToList();
-        _dataDict[nameof(AppDbContext.MuwaqqitPrayerTimes)] = appDbContext.MuwaqqitPrayerTimes.AsNoTracking().ToList().OfType<object>().ToList();
-        _dataDict[nameof(AppDbContext.Profiles)] = appDbContext.Profiles.AsNoTracking().ToList().OfType<object>().ToList();
-        _dataDict[nameof(AppDbContext.ProfileConfigs)] = appDbContext.ProfileConfigs.AsNoTracking().ToList().OfType<object>().ToList();
-        _dataDict[nameof(AppDbContext.ProfileLocations)] = appDbContext.ProfileLocations.AsNoTracking().ToList().OfType<object>().ToList();
+        fillDataDict();
 
         TableOptions = _dataDict.Select(x => x.Key).ToList();
         SelectedTableOption = TableOptions[0];
@@ -45,4 +38,37 @@ public partial class DatabaseTablesPageViewModel(
 
         OnChangeSelectionAction?.Invoke(value);
     }
+
+    private void fillDataDict()
+    {
+        List<PropertyInfo> dbSetPropertyInfos = typeof(AppDbContext)
+            .GetProperties()
+            .Where(x => x.PropertyType.IsGenericType &&
+                        x.PropertyType.GetGenericTypeDefinition() == typeof(DbSet<>))
+            .ToList();
+
+        foreach (PropertyInfo dbSetPropertyInfo in dbSetPropertyInfos)
+        {
+            Type entityTypeOfDbSet = dbSetPropertyInfo.PropertyType.GetGenericArguments()[0];
+            MethodInfo setMethod = typeof(DbContext).GetMethod(nameof(DbContext.Set), Type.EmptyTypes);
+            MethodInfo genericSet = setMethod.MakeGenericMethod(entityTypeOfDbSet);
+            object dbSet = genericSet.Invoke(appDbContext, null);
+
+            var asNoTrackingMethod = typeof(EntityFrameworkQueryableExtensions)
+                    .GetMethods(BindingFlags.Public | BindingFlags.Static)
+                    .First(m => m.Name == nameof(EntityFrameworkQueryableExtensions.AsNoTracking)
+                                && m.GetParameters().Length == 1)
+                    .MakeGenericMethod(entityTypeOfDbSet);
+            object dbSetWithAsNoTracking = asNoTrackingMethod.Invoke(null, [dbSet]);
+
+            MethodInfo toListMethod = typeof(Enumerable)
+                .GetMethod(nameof(Enumerable.ToList))
+                .MakeGenericMethod(entityTypeOfDbSet);
+
+            object dbSetFullResult = toListMethod.Invoke(null, [dbSetWithAsNoTracking]);
+
+            _dataDict[dbSetPropertyInfo.Name] = ((IEnumerable<object>)dbSetFullResult).ToList();
+        }
+    }
+
 }
