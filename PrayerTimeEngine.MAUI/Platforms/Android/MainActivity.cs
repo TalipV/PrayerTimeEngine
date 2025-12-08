cf58c8c7729c8696f4fbd2a34b3f4e4ce843e91b
talip <talip.vu@gmail.com>
2025-12-08 23:26:42 +0100

    only allow portrait mode

--- PrayerTimeEngine.MAUI/Platforms/Android/MainActivity.cs
+++ PrayerTimeEngine.MAUI/Platforms/Android/MainActivity.cs
@@ -5,7 +5,17 @@ using PrayerTimeEngine.Platforms.Android.Notifications;
 
 namespace PrayerTimeEngine.Platforms.Android;
 
-[Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
+[Activity(
+    Theme = "@style/Maui.SplashTheme", 
+    MainLauncher = true, 
+    ConfigurationChanges = ConfigChanges.ScreenSize 
+                        | ConfigChanges.Orientation 
+                        | ConfigChanges.UiMode 
+                        | ConfigChanges.ScreenLayout 
+                        | ConfigChanges.SmallestScreenSize 
+                        | ConfigChanges.Density,
+    ScreenOrientation = ScreenOrientation.Portrait
+)]
 public class MainActivity : MauiAppCompatActivity
 {
     public static MainActivity Instance { get; private set; }
